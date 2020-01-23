2e21b9ae93dd5a5978e39b0bc51e8828a2cbd545
mge <m@g12n.de>
2020-01-23 14:37:54 +0100

    basic 11ty setup

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dir: {
+    input: "source",
+    output: "docs"
+  },
+  pathPrefix: "/svg-cards/",
+  templateFormats: ["md", "css", "njk", "11ty.js"],
+  passthroughFileCopy: true
+};
