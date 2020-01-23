f61bb8a3d495cf42e2dd671eb5cbbf275cb9fd4e
mge <michael@gehrmann-design.de>
2020-01-24 00:22:41 +0100

    iteration ace of diamonds

--- source/cards/d-01.11ty.js
+++ source/cards/d-01.11ty.js
@@ -6,13 +6,17 @@ const cy = height / 2;
 const step = 30;
 const halfStep = step/2;
 let content = ``
-content += `<rect width="${width}" height="${height}" />`;
 
+// Set Background Rectangle
+content += `<rect width="${width}" height="${height}" fill="hsl(10,100%, 10%)" />`;
 
-for (let i=1; i<=16; i++){
-    content += `<rect x="${cx - i * halfStep}" y="${cy -
-      i * halfStep}" width="${i * step}" height="${i *
-      step}" fill="rgba(255,255,255,0.1)" transform="rotate(${i*4},${cx}, ${cy})"/>`;
+for (let i=1; i<=12; i++){
+    let  x= cx - i * halfStep
+    let  y= cy - i * halfStep
+    let  w= i * step
+    let h= i *step
+    let fill=`hsla(10,60%, 60%, 0.1)`
+    content += `<rect x="${x}" y="${y}" width="${w}" height="${h}" fill="${fill}" transform="rotate(${i*4},${cx}, ${cy})"/>`;
 }
 
 module.exports = `<svg viewBox="${viewBox}" width="${width}" height="${height}" version="1.1" xmlns="http://www.w3.org/2000/svg">${content}</svg>`;
